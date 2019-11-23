85f79039036025b2ac422393f01f59707d95f315
Kerem <base@Kerems-MacBook-Pro.local>
2019-11-23 19:46:10 +0100

    DB create/delete/show implemented

--- FlaskRestfullApp/app.py
+++ FlaskRestfullApp/app.py
@@ -1,24 +1,71 @@
-from flask import Flask
-from flask_restful import Resource, Api
-from db import db_ops
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from db import db_ops, create_db, get_dbs, delete_db, write_data
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+# parser.add_argument('user', type=str, help='Username of the newly to be created user')
+# parser.add_argument('password', type=str, help='Password')
+parser.add_argument('db_name', type=str, help='Name of the new database')
+# parser.add_argument('data_to_be_written', location='form', help='The data that wants to be written to a database')
 
-class HelloWorld(Resource):
+class NewDb(Resource):
+    
+    def post(self):
+        args = parser.parse_args()
+        # Error checking
+        if args['db_name'] is None:
+            return {'Error': 'Please enter a db_name'}
+
+        db_name = args['db_name']
+        result = create_db(db_name)
+        return result
+
+class GetDBs(Resource):
     def get(self):
-        return {'hello': 'Kerem',
-                'sonunda': 'Sonuc aldin'}
+        dbs = get_dbs()
+        return {"Existing Databases": dbs}
+
+class DeleteDB(Resource):
+    def post(self):
+        args = parser.parse_args()
+
+        if args['db_name'] is None:
+            return {'Error': 'Please enter a db_name'}
+        db_name = args['db_name']   
+        result = delete_db(db_name)
+        return "Massega is received"
+
 
 class WriteData(Resource):
-    def get(self):
-        x = db_ops()
-        return {'veriler' : 'yazildi'}
+
+    def __init__(self):
+        self.reqparser = reqparse.RequestParser()
+        self.reqparser.add_argument('db_name', type=str, help='Name of the database')
+        self.reqparser.add_argument('data', type=str, location='json')
+        super(WriteData, self).__init__()
+
+    def post(self):
+        # How to read the body of the request
+        args = self.reqparser.parse_args()
+        # data_tbw = args['data_to_be_written']
+        db_name = args['db_name']
+
+        # The data to be written into a database
+        json_data = request.get_json()
+
+        # Right now, I can read the data from a post request
+        # TODO: Write the data into a database.
+
+        return True
 
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(WriteData, '/yaz')
+api.add_resource(NewDb, '/newdb')
+api.add_resource(GetDBs, '/getdbs')
+api.add_resource(DeleteDB, '/deletedb')
+api.add_resource(WriteData, '/writedata')
 
 
 if __name__ == '__main__':
--- FlaskRestfullApp/db.py
+++ FlaskRestfullApp/db.py
@@ -1,5 +1,8 @@
 from influxdb import InfluxDBClient
 
+client = InfluxDBClient('db', 8086, 'root', 'root', 'example')
+
+
 def db_ops():
 
     json_body = [
@@ -22,3 +25,65 @@ def db_ops():
 
     print("Results are ", result)
     return str(result)
+
+    # Check whether the db already exists
+
+def create_db(db_name):
+
+    # Just to get the name of the databases, insead of a another data structure
+    dbs = get_dbs()
+    if db_name in dbs:
+        return "Already exists"
+
+    try:
+        client.create_database(db_name)
+    except:
+        return "Something went wrong during creating a new database"
+
+
+    return "A new db with the name " + db_name + " created"
+
+def get_dbs():
+
+    try:
+        databases = client.get_list_database()
+    except:
+        return "Something went wrong during reading list of databases"
+
+
+    dbs = [key['name'] for key in databases] 
+    return dbs
+
+def delete_db(db_name):
+    
+    # check whether the db already exits
+    dbs = get_dbs()
+    if db_name not in dbs:
+        return "DB does not exist!"
+    
+    try:
+        client.drop_database(db_name)
+    except:
+        print("Something went wrong")
+        return "Something went wrong during deletion of database!"
+    
+    return "Database deleted successfully"
+
+
+    #If yes, then delete the db
+
+def write_data(data_points, db_name):
+
+    return True
+
+# Sample Data Format
+# {
+# 		"thread": 1,
+# 		"priority": 80,
+# 		"I": 200,
+# 		"C": 500,
+# 		"max_latency": 10,
+# 		"min_latency": 10,
+# 		"avg_latency": 10,
+# 		"act_latency": 10
+# 	}
